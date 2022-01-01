ace59abf8bd91770c89b1bf265ea4fa81e5c04ff
Jai jc <79501478+JCjaiRN@users.noreply.github.com>
2022-01-01 12:24:00 +0530

    add files via upload
    ES6  --- in this commit i have gain knowledge about echmascript 6in javascript

---
+++ set-default-parameters-for-your-functions.js
@@ -0,0 +1,5 @@
+ // Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+console.log(increment(1,));
+console.log(increment());
---
+++ write-arrow-functions-with-parameters.js
@@ -0,0 +1,2 @@
+const myConcat = (arr1, arr2) => arr1.concat(arr2); 
+console.log(myConcat([1, 2], [3, 4, 5]));
