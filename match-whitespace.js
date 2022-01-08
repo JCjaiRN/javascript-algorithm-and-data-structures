3d5e0b3516dbdc0fe29c076cb3c2d0380435d9c8
Jayachandran <79501478+JCjaiRN@users.noreply.github.com>
2022-01-08 22:09:24 +0530

    added files via upload
    day 06 of 100days coding

---
+++ match-whitespace.js
@@ -0,0 +1,5 @@
+let sample = "Whitespace is important in separating words";
+let countWhiteSpace = /\s/g; // Change this line
+let result = sample.match(countWhiteSpace);
+
+console.log( result = sample.match(countWhiteSpace));
