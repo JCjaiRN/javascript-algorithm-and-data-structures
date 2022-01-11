68d02d8aa5576976b3d30fd15c109b26d853b8ec
Jayachandran <79501478+JCjaiRN@users.noreply.github.com>
2022-01-11 19:02:44 +0530

    Add files via upload

---
+++ Functional  Programming/implement-map-on-a-prototype.js
@@ -0,0 +1,16 @@
+// The global variable
+const s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  const newArray = [];
+  // Only change code below this line
+for(let i = 0; i < this.length; i++){
+  newArray.push(callback(this[i]));
+}
+  // Only change code above this line
+  return newArray;
+};
+
+const new_s = s.myMap(function(item) {
+  return item * 2;
+});
---
+++ Functional  Programming/use-the-some-method-to-check-that-any-elements-in-an-array-meet-a-criteria.js
@@ -0,0 +1,10 @@
+function checkPositive(arr) {
+  // Only change code below this line
+return arr.some(elem => elem > 0);
+
+  // Only change code above this line
+}
+
+checkPositive([1, 2, 3, -4, 5]);
+
+console.log(checkPositive([1, 2, 3, -4, 5]))
